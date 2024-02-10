6b58194cfdfd15887748aa1c57223380f27bad44
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-10 22:34:04 +0100

    * feat(app.module.ts): add UsersModule and ConfigModule to imports * feat(app.module.ts): add UsersController to controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
