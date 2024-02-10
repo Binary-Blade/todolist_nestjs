faac39e924dc18ebe6aff38525bb62e9112ea8d3
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-10 23:37:27 +0100

    * feat(app.module.ts): add support for TypeORM configuration using ConfigService and ConfigModule * fix(app.module.ts): fix envFilePath in ConfigModule.forRoot to use correct path

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './modules/users/users.controller';
 import { UsersModule } from './modules/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: ['../.env.development'],
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'), // Database host
+          port: config.get<number>('DB_PORT'), // Database port
+          username: config.get<string>('DB_USERNAME'), // Database username
+          password: config.get<string>('DB_PASSWORD'), // Database password
+          database: config.get<string>('DB_NAME'), // Database name
+          entities: [__dirname + '**/**/*.entity{.ts,.js}'], // Path to your entities
+          synchronize: config.get<boolean>('DB_SYNCHRONIZE'), // Synchronize the database state with the models on application startup
+        };
+      },
     }),
   ],
   controllers: [AppController, UsersController],
