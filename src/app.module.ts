0a2c76ba4937e71efd1d53449a89471c44d3bdff
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-12 03:21:47 +0100

    * chore(app.module.ts): update entities configuration in TypeORM module * feat(app.module.ts): import User entity from users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
+import { User } from './modules/users/entities/user.entity';
 // import { CategoriesModule } from './modules/categories/categories.module';
 // import { TasksModule } from './modules/tasks/tasks.module';
 // import { AuthModule } from './modules/auth/auth.module';
@@ -23,7 +24,7 @@ import { UsersModule } from './modules/users/users.module';
           username: config.get<string>('DB_USERNAME'), // Database username
           password: config.get<string>('DB_PASSWORD'), // Database password
           database: config.get<string>('DB_NAME'), // Database name
-          entities: [__dirname + 'src/modules/**/**/*.entity{.ts}'], // Path to your entities
+          entities: [User], // Path entities
           synchronize: config.get<boolean>('DB_SYNCHRONIZE'), // Synchronize the database state with the models on application startup
         };
       },
