5077da5171a025869f154029b7d3f46a863b0eeb
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-13 05:04:36 +0100

    * feat(auth): Create Signup method with argon2 hash

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly userService: UsersService) { }
+
+  @Post('/signup')
+  async create(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
+  }
+
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as argon2 from 'argon2';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) { }
+  // TODO: Make Nethod : Signup, Login, Logout
+
+  /**
+   * Asynchronously signs up a new user.
+   * 
+   * @param email The email address of the user attempting to sign up.
+   * @param password The password provided by the user, to be hashed for storage.
+   * @returns The created user object.
+   * @throws BadRequestException if an account with the given email already exists.
+   */
+
+  async signup(email: string, password: string) {
+
+    const usersExist = await this.usersService.findEmail(email);
+
+    if (usersExist.length) {
+      throw new BadRequestException('Account already exists');
+    }
+
+    const passwordHashed = await argon2.hash(password);
+    const user = await this.usersService.create(email, passwordHashed);
+
+    return user;
+  }
+}
