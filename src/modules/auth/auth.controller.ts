db7db51355a69339f4c64727e5e2f050590f30d7
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-16 09:00:20 +0100

    * feat(auth.controller): Add route login and refresh-token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,8 +4,9 @@ import {
   Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LoginDTO } from './dto/login.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,4 +17,13 @@ export class AuthController {
     return this.authService.signup(body.email, body.password, body.username);
   }
 
+  @Post('/login')
+  async login(@Body() loginDto: LoginDTO) {
+    return await this.authService.login(loginDto);
+  }
+
+  @Post('/refresh-token')
+  async refreshToken(@Body() { refreshToken }: RefreshTokenDto) {
+    return await this.authService.refreshToken(refreshToken);
+  }
 }
