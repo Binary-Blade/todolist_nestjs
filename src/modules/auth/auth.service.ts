fc561965d864acd63d169778f191fcad8398fd19
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-16 08:59:51 +0100

    * feat(auth.service): Add methods, login, refreshToken, getToken for authentification

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,31 +1,28 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as argon2 from 'argon2';
+import { LoginDTO } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { User } from '../users/entities/user.entity';
 
+export interface JWTTokens {
+  token: string;
+  refreshToken: string;
+}
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) { }
-
-  private hashPassword(password: string): Promise<string> {
-    return argon2.hash(password)
-  }
-
-
-  /**
-   * Asynchronously signs up a new user.
-   * 
-   * @param email The email address of the user attempting to sign up.
-   * @param password The password provided by the user, to be hashed for storage.
-   * @returns The created user object.
-   * @throws BadRequestException if an account with the given email already exists.
-   */
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private configService: ConfigService
+  ) { }
 
   async signup(email: string, password: string, username?: string) {
+    const userExist = await this.usersService.findEmail(email);
 
-    const usersExist = await this.usersService.findEmail(email);
-
-    if (usersExist.length) {
+    if (userExist) {
       throw new BadRequestException('Account already exists');
     }
 
@@ -36,9 +33,63 @@ export class AuthService {
       username
     });
 
-    return user;
+    // Exclude sensitive fields before returning
+    const { password: _, ...result } = user;
+    return result;
   }
 
+  async login(loginDto: LoginDTO): Promise<JWTTokens> {
+    const { email, password } = loginDto;
+    const user = await this.usersService.findEmail(email);
 
+    if (user) {
+      const validPassword = await argon2.verify(user.password, password);
+
+      if (!validPassword) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      return this.getTokens(user)
+    }
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  async refreshToken(token: string): Promise<JWTTokens> {
+    try {
+
+      const { sub: email } = await this.jwtService.verify(token, {
+        secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET')
+      });
+
+      const user = await this.usersService.findEmail(email);
+      return this.getTokens(user);
+
+    } catch (err) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+  }
+
+  private hashPassword(password: string): Promise<string> {
+    return argon2.hash(password)
+  }
+
+  private async getTokens(user: User): Promise<JWTTokens> {
+    const [token, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(
+        { sub: user.email },
+        {
+          secret: this.configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+          expiresIn: this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRATION'),
+        }
+      ),
+      this.jwtService.signAsync(
+        { sub: user.email },
+        {
+          secret: this.configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+          expiresIn: this.configService.get<string>('JWT_REFRESH_TOKEN_EXPIRATION'),
+        }
+      ),
+    ]);
+    return { token, refreshToken }
+  }
 
 }
