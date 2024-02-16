979a5d3e346a30216de0ce572dcdb4e975f768a6
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-16 08:58:23 +0100

    * feat(auth/dto): Add class validator dto login and create refreshtoken.dto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class LoginDTO {
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
