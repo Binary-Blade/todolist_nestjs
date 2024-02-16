b2110884283aab23fa26d25b1036ea5a70a4d33d
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-16 03:50:00 +0100

    * feat(task.entity): Create TaskEntity

--- src/modules/tasks/entities/task.entity.ts
+++ src/modules/tasks/entities/task.entity.ts
@@ -1,1 +1,28 @@
-export class Task {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './../../users/entities/user.entity';
+import { Category } from './../../categories/entities/category.entity';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn('increment')
+  taskId: number;
+
+  @ManyToOne(() => User, (user) => user.tasks)
+  user: User;
+
+  @ManyToOne(() => Category, (category) => category.tasks)
+  category: Category;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'varchar' })
+  status: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  deadline: Date;
+}
+
