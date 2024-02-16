6ce7b3e42e433a4b49beb61653e0bf4515a31e0a
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-16 03:49:31 +0100

    * feat(category.entity): Create CategoryEntity

--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,23 @@
-export class Category {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { User } from './../../users/entities/user.entity';
+import { Task } from './../../tasks/entities/task.entity';
+
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn('increment')
+  categoryId: number;
+
+  @ManyToOne(() => User, (user) => user.categories)
+  user: User;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @OneToMany(() => Task, (task) => task.category)
+  tasks: Task[];
+}
+
