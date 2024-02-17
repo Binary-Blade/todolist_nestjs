e1c513bff09a04e25d049f299b638c20a14803c6
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-17 01:05:13 +0100

    * feat(auth.module): Add AccessTokenStrategy in providers authmodule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AccessTokenStrategy } from './passport-strategy/access-token.strategy';
 
 @Module({
   imports: [JwtModule.register({}), UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AccessTokenStrategy],
 })
 export class AuthModule { }
