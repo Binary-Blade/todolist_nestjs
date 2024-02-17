057ad7e75e42294b4b8ce282dca79cb42d9f2a0a
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-17 01:03:11 +0100

    * refactor(auth.service): Delete HttpException

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as argon2 from 'argon2';
 import { LoginDTO } from './dto/login.dto';
