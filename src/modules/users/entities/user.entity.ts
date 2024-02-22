ee7173cc199985939e6ec9280d1ed9ea444835a3
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-22 05:21:37 +0100

    * feat(user.entity): Add Role logic entity for ADMIN role

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -2,19 +2,23 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Task } from './../../tasks/entities/task.entity';
 import { Category } from './../../categories/entities/category.entity';
 
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin'
+}
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('increment')
   userId: number;
 
   @Column({ type: 'varchar' })
-  username: string;
-
-  @Column({ type: 'varchar' })
   email: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', nullable: false })
   password: string;
+
+  @Column({ type: 'varchar', default: UserRole.USER, name: 'userRole' })
+  role: UserRole;
 
   @OneToMany(() => Task, (task) => task.user)
   tasks: Task[];
