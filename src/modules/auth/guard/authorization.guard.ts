2b58b8ccab44802d461733bb58212f5849c5afa8
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-22 06:48:36 +0100

    * feat(authorization.guard): create RoleGuard for authorization based on role logic

--- src/modules/auth/guard/authorization.guard.ts
+++ src/modules/auth/guard/authorization.guard.ts
@@ -1,1 +1,24 @@
-// TODO: Make authorization.guard
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { UserRole } from "src/modules/users/entities/user.entity";
+import { ROLE_KEY } from "../decorators/role";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRole = this.reflector.getAllAndOverride<UserRole>(
+      ROLE_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]
+    )
+    if (!requiredRole) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    return user.role === requiredRole;
+  }
+}
