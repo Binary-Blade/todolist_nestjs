253214cd0566e65a41654f6613d839e3abce4880
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-23 09:43:44 +0100

    * feat(categories.controller): add request for take id user with guard AccessTokenGuard

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -5,6 +5,7 @@ import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 
+@UseGuards(AccessTokenGuard)
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('categories')
 export class CategoriesController {
@@ -12,27 +13,26 @@ export class CategoriesController {
 
   @Post('create')
   create(@Body() createCategoryDto: CreateCategoryDto, @Request() req: any) {
-    return this.categoriesService.create(createCategoryDto, req.user.id);
+    return this.categoriesService.create(createCategoryDto, req.user.userId);
   }
 
-  @UseGuards(AccessTokenGuard)
   @Get('/findAll')
-  findAll() {
-    return this.categoriesService.findAll();
+  findAll(@Request() req: any) {
+    return this.categoriesService.findAll(req.user);
   }
 
   @Get('/:id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
+  findOne(@Request() req: any, @Param('id') id: string) {
+    return this.categoriesService.findOne(req.user, +id);
   }
 
   @Patch('/:id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(+id, updateCategoryDto);
+  update(@Request() req: any, @Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return this.categoriesService.update(req.user, +id, updateCategoryDto);
   }
 
   @Delete('/:id')
-  remove(@Param('id') id: string) {
-    return this.categoriesService.remove(+id);
+  remove(@Request() req: any, @Param('id') id: string) {
+    return this.categoriesService.remove(req.user, +id);
   }
 }
