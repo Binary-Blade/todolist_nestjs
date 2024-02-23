354891f0f4c5ad481387e9fe61bc48133452d0c7
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-23 17:10:57 +0100

    * feat(create-task.dto): create 5 type for createTaskDto

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTaskDto {}
+import { Status } from "../enum/status.enum";
+
+export class CreateTaskDto {
+  readonly title: string;
+  readonly description?: string;
+  readonly status: Status;
+  readonly active: boolean;
+  readonly categoryId?: number;
+}
