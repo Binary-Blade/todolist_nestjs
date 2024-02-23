1d9b6d6b845aa047ed07756aec88055d51f6d77d
Binary-Blade <119490047+Binary-Blade@users.noreply.github.com>
2024-02-23 17:14:56 +0100

    * feat(tasks.module): add TypeOrmModule with User, Category, Task entity for task module

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { Category } from '../categories/entities/category.entity';
+import { Task } from './entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Category, Task])],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [TasksService]
