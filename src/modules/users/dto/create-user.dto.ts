da3444113b32e9901ffa4359b47c8dc9cc5d55f1
Binary Blade <binary@binarys-macbook-pro.home>
2024-02-26 19:08:04 +0100

    * feat(create-user.dto): Add decorators from class-validator

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,7 +1,16 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword, MinLength } from "class-validator";
 import { UserRole } from "../entities/user.entity";
 
 export class CreateUserDto {
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(5)
   readonly email: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
   readonly password: string;
+
   readonly role?: UserRole;
 }
