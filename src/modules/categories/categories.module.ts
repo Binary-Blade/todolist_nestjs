ce7fb4079d7c3132953d532f8359db740a9e431e
Binary Blade <binary@binarys-macbook-pro.home>
2024-02-26 19:07:14 +0100

    * feat(categories.module): Import TasksModule

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -4,9 +4,10 @@ import { CategoriesController } from './categories.controller';
 import { Category } from './entities/category.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { TasksModule } from '../tasks/tasks.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category, User])],
+  imports: [TypeOrmModule.forFeature([Category, User]), TasksModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService]
