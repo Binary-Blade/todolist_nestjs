12da1a4a682980bf538641038c9415b270b89b7c
Binary Blade <binary@binarys-macbook-pro.home>
2024-02-26 19:07:44 +0100

    * feat(create-task.dto): Add decorators from class-validator

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,9 +1,23 @@
+import { IsBoolean, IsEnum, IsNotEmpty, IsNumberString, IsOptional, IsString, MinLength } from "class-validator";
 import { Status } from "../enum/status.enum";
 
 export class CreateTaskDto {
+
+  @IsNotEmpty()
+  @MinLength(1)
+  @IsString()
   readonly title: string;
+
+  @IsOptional()
+  @IsString()
   readonly description?: string;
+
+  @IsEnum(Status)
   readonly status: Status;
+
+  @IsBoolean()
   readonly active: boolean;
+
+  @IsNumberString()
   readonly categoryId?: number;
 }
